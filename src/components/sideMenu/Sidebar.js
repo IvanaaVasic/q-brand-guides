4a862d9e5b2428e57191e4354887c9f54ace04be
IvanaaVasic <ivana.p.vasic@gmail.com>
2022-05-25 14:20:08 +0200

    side bar data moved to config folder

--- src/components/sideMenu/Sidebar.js
+++ src/components/sideMenu/Sidebar.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import MenuCSS from "./Menu.module.css";
-import { SidebarDataPresentations } from "./SidebarDataPresentations";
-import { SidebarDataDesign } from "./SideBarDataDesign";
-import { SidebarDataBrand } from "./SideBarDataBrand";
+import { SidebarDataPresentations } from "../../config/sidebarData/SidebarDataPresentations";
+import { SidebarDataDesign } from "../../config/sidebarData/SideBarDataDesign";
+import { SidebarDataBrand } from "../../config/sidebarData/SideBarDataBrand";
+import { SidebarSmallNav } from "../../config/sidebarData/SideBarSmallNav";
 import Logo from "../../assets/Logo.png";
 import HederNavContainer from "./HeaderNavContainer";
 
@@ -15,19 +16,19 @@ const Sidebar = () => {
         <img src={Logo} alt="Logo" className={MenuCSS.logo} />
         <HederNavContainer
           data={SidebarDataBrand}
-          smallNavTTitle={"THE BRAND"}
+          smallNavTTitle={SidebarSmallNav.brand}
           setupActiveId={setActiveId}
           activeState={activeId}
         />
         <HederNavContainer
           data={SidebarDataDesign}
-          smallNavTTitle={"DESIGN"}
+          smallNavTTitle={SidebarSmallNav.design}
           setupActiveId={setActiveId}
           activeState={activeId}
         />
         <HederNavContainer
           data={SidebarDataPresentations}
-          smallNavTTitle={"PRESENTATIONS"}
+          smallNavTTitle={SidebarSmallNav.presentation}
           setupActiveId={setActiveId}
           activeState={activeId}
         />
---
+++ src/config/sidebarData/SideBarSmallNav.js
@@ -0,0 +1,5 @@
+export const SidebarSmallNav = {
+  brand: "THE BRAND",
+  design: "DESIGN",
+  presentation: "PRESENTATIONS",
+};
